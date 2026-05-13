b23bd32a01ba7009184a8d5c5db89c8520be5b27
sahil-inteligenai <sahil@inteligenai.com>
2026-05-13 17:58:40 +0530

    Refactor SecurityConfig to add @Qualifier annotation for corsConfigurationSource in filterChain method

--- api/src/main/java/com/sahilkumar/api/security/SecurityConfig.java
+++ api/src/main/java/com/sahilkumar/api/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.sahilkumar.api.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,7 +31,8 @@ public class SecurityConfig {
     private final UserDetailsService userDetailsService;
 
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity http, CorsConfigurationSource cors) throws Exception {
+    SecurityFilterChain filterChain(HttpSecurity http,
+                                    @Qualifier("corsConfigurationSource") CorsConfigurationSource cors) throws Exception {
         http
                 .cors(c -> c.configurationSource(cors))
                 .csrf(csrf -> csrf.disable())
